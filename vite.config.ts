ca9ae45785911c6c2a2d262b75cfc00fbf89833f
Rob Lingle <rob@nonesuch.dev>
2025-11-24 16:01:12 -0500

    Updates to get deployment working on CloudFlare Workers

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,11 @@ const config = defineConfig({
     tanstackStart(),
     viteReact(),
   ],
+  build: {
+    rollupOptions: {
+      external: ['cloudflare:workers'],
+    },
+  },
 })
 
 export default config
